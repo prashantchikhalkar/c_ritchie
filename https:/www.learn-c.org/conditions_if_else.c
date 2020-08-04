7de98ff1adca347e2b40f075f8fd2b08efb5130d
prashant <prashantchikhalkar@gmail.com>
2020-08-04 21:46:25 +0530

    exercise : Conditions

---
+++ https:/www.learn-c.org/conditions_if_else.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void guessNumber(int guess) {
+    // TODO: write your code here
+    if (guess == 555) { 
+        printf ("Correct. You guessed it!\n");
+    }
+    if (guess < 555) { 
+        printf ("Your guess is too low.\n");
+    }
+    if (guess > 555) {
+        printf ("Your guess is too high.\n");
+    }
+}
+
+int main() {
+    guessNumber(500);
+    guessNumber(600);
+    guessNumber(555);
+}
