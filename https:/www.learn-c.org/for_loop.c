29b1f9995a0fb01be0a6c97ebc94b612afdd0e11
prashant <prashantchikhalkar@gmail.com>
2020-08-06 22:15:55 +0530

    create for_loop.c

---
+++ https:/www.learn-c.org/for_loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  int factorial = 1;
+  int i;
+
+  /* calculate the factorial using a for loop  here*/
+   
+
+  printf("10! is %d.\n", factorial);
+}
