be8dc9b8c6d61a12b7797985042fe1cf13d99e72
prashant <prashantchikhalkar@gmail.com>
2020-08-25 22:08:49 +0530

    complete array_pointers.c

--- https:/www.learn-c.org/array_pointers.c
+++ https:/www.learn-c.org/array_pointers.c
@@ -4,15 +4,16 @@
 int main() {
     int i, j;
     /* TODO: define the 2D pointer variable here */
-    int nrows = 3;
-    int ncols = 6;
+    int **pnumbers;
 
     /* TODO: complete the following line to allocate memory for holding three rows */
-    pnumbers = (int **) malloc();
+    pnumbers = (int **) malloc(3 * sizeof (int *));
 
     /* TODO: allocate memory for storing the individual elements in a row */
     pnumbers[0] = (int *) malloc(1 * sizeof(int));
-
+    pnumbers[1] = (int *) malloc(2 * sizeof(int));
+	pnumbers[2] = (int *) malloc(3 * sizeof(int));
+    
     pnumbers[0][0] = 1;
     pnumbers[1][0] = 1;
     pnumbers[1][1] = 1;
@@ -29,9 +30,11 @@ int main() {
 
     for (i = 0; i < 3; i++) {
         /* TODO: free memory allocated for each row */
+        free (pnumbers[i]);
     }
 
     /* TODO: free the top-level pointer */
-
+	free (pnumbers);
+    
   return 0;
 }
